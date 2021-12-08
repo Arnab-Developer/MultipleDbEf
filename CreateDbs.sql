78d7dfef9f62b1d4f90ccc711f1467ae9676c3ab
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-12-08 11:50:21 +0530

    Create DB script

---
+++ CreateDbs.sql
@@ -0,0 +1,40 @@
+﻿USE [master]
+GO
+
+CREATE DATABASE StudentDb
+ON PRIMARY
+( 
+    NAME = N'StudentDb', 
+    FILENAME = N'C:\db\StudentDb.mdf' , 
+    SIZE = 8192KB , 
+    MAXSIZE = UNLIMITED, 
+    FILEGROWTH = 65536KB 
+)
+LOG ON 
+( 
+    NAME = N'StudentDbLog', 
+    FILENAME = N'C:\db\StudentDb.ldf' , 
+    SIZE = 8192KB , 
+    MAXSIZE = 2048GB , 
+    FILEGROWTH = 65536KB 
+)
+GO
+
+CREATE DATABASE PersonDb
+ON PRIMARY
+( 
+    NAME = N'PersonDb', 
+    FILENAME = N'C:\db\PersonDb.mdf' , 
+    SIZE = 8192KB , 
+    MAXSIZE = UNLIMITED, 
+    FILEGROWTH = 65536KB 
+)
+LOG ON 
+( 
+    NAME = N'PersonDbLog', 
+    FILENAME = N'C:\db\PersonDb.ldf' , 
+    SIZE = 8192KB , 
+    MAXSIZE = 2048GB , 
+    FILEGROWTH = 65536KB 
+)
+GO
